bf9c4e24a83a45c12c44b350dfb4362bfd02c487
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-05-07 22:42:57 +0545

    Added banner

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,18 +3,14 @@ import { BOOKS } from "@/data/books-data";
 // components
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import Banner from "@/components/layout/banner";
 import BookCard from "@/components/custom/book-card";
 
 export default function Home() {
   return (
     <div className=" h-ful px-[var(--px)] sm:px-[var(--sm-px)] flex flex-col " >
       <Navbar />
-
-      <div className="h-[50vh] rounded-lg flex flex-col justify-center items-center">
-        <p className="text-center">One stop for all hindu religious books. </p>
-        <p className="text-center">We have a collection of books on various topics like Ramayan, Mahabharat, Bhagwat Geeta, Vedas, Upanishads, Puranas, etc.</p>
-      </div>
-
+      <Banner />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {BOOKS.map((book, index) => (<BookCard key={index} book={book} />))}
       </div>
---
+++ src/components/layout/banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className="h-[80vh] rounded-lg flex flex-col justify-center items-center">
+            <p className="text-center text-xl">One stop for all hindu religious books. </p>
+            <p className="text-center text-2xl">We have a collection of books on various topics like Ramayan, Mahabharat, Bhagwat Geeta, Vedas, Upanishads, Puranas, etc.</p>
+        </div>
+    )
+}
+
+export default Banner;
